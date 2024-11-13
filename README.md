0c235d541b2aaded730711e8c3b47c1d0a13a15c
tanghaojie <thj3360016@gmail.com>
2024-11-13 10:54:44 +0800

    feat: Readme.

--- README.md
+++ README.md
@@ -2,6 +2,64 @@
 
 Word cloud in tunnel.
 
+## Install
+
 ```sh
-npm i vuewordcloud
+npm i @jtis/wordcloudtunnel
+pnpm i @jtis/wordcloudtunnel
+```
+
+## Usage
+
+```html
+const el = ref<InstanceType<typeof WordCloudTunnel>>()
+
+let id = 0;
+onMounted(() => {
+  timer = window.setInterval(() => {
+    el.value?.append({
+      id: (id++).toString(),
+      text: 'text',
+    })
+  }, 1000)
+})
+
+<WordCloudTunnel
+  ref="el"
+  :color="['#ffd077', '#3bc4c7', '#3a9eea', '#ff4e69', '#461e47']"
+  :axisCount="6"
+  :randomAngleRangePerAxis="60"
+  :randomAxis="false"
+  :scaleStep="0.15"
+  :animation-duration="1000"
+></WordCloudTunnel>
+```
+
+```html
+const el = ref<InstanceType<typeof WordCloudTunnel>>()
+
+let id = 0;
+onMounted(() => {
+  timer = window.setInterval(() => {
+    el.value?.append({
+      id: (id++).toString(),
+      text: 'text',
+    })
+  }, 1000)
+})
+
+<WordCloudTunnel
+  ref="el"
+  :color="['#ffd077', '#3bc4c7', '#3a9eea', '#ff4e69', '#461e47']"
+  :axisCount="6"
+  :randomAngleRangePerAxis="60"
+  :randomAxis="false"
+  :scaleStep="0"
+  :opacityStep="0"
+  :animation-duration="1000"
+>
+  <template #default="{id, level, text, color}">
+    <div :style="{backgroundColor: level > 1 ? 'green' : 'gray',}">{{ text }}</div>
+  </template>
+</WordCloudTunnel>
 ```
--- dev/App.vue
+++ dev/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue'
+import { CSSProperties, onMounted, onUnmounted, ref } from 'vue'
 import { WordCloudTunnel } from '../src/main'
 
 const el = ref<InstanceType<typeof WordCloudTunnel>>()
 const el2 = ref<InstanceType<typeof WordCloudTunnel>>()
+const el3 = ref<InstanceType<typeof WordCloudTunnel>>()
+const el4 = ref<InstanceType<typeof WordCloudTunnel>>()
 
 const words = [
   'lay',
@@ -59,7 +61,7 @@ const words = [
 ]
 
 let index = 0
-function randomAdd() {
+function handAdd() {
   const item = {
     id: (index++).toString(),
     text: words[Math.floor(Math.random() * words.length)],
@@ -71,44 +73,113 @@ let index2 = 0
 let timer: number
 onMounted(() => {
   timer = window.setInterval(() => {
-    const item = {
+    el2.value?.append({
       id: (index2++).toString(),
       text: words[Math.floor(Math.random() * words.length)],
-    }
-    el2.value?.append(item)
+    })
+    el3.value?.append({
+      id: (index2++).toString(),
+      text: words[Math.floor(Math.random() * words.length)],
+      customPayload: {
+        desc: 'Item is customed',
+      },
+    })
+    el4.value?.append({
+      id: (index2++).toString(),
+      text: words[Math.floor(Math.random() * words.length)],
+    })
   }, 1000)
 })
 
 onUnmounted(() => {
   clearInterval(timer)
 })
+
+function customItemStyle(item: any) {
+  console.log(item)
+
+  return {
+    color: '#fff',
+    scale: item.level > 1 ? 0.3 : 1,
+    'font-size': '28px',
+    transition: 'all 3s ease',
+    backgroundColor: item.level > 1 ? 'green' : 'gray',
+    padding: '4px 16px',
+    borderRadius: '8px',
+  } as CSSProperties
+}
 </script>
 
 <template>
-  <div style="width: 600px; height: 400px; border: solid #ddd 1px">
-    <WordCloudTunnel
-      ref="el2"
-      :color="['#ffd077', '#3bc4c7', '#3a9eea', '#ff4e69', '#461e47']"
-      :axisCount="6"
-      :randomAngleRangePerAxis="60"
-      :randomAxis="false"
-      :scaleStep="0.15"
-      :animation-duration="1000"
-    ></WordCloudTunnel>
+  <div style="width: 100%; height: 400px; display: flex; flex-direction: row">
+    <div style="flex: 1 1 auto; border: solid #ddd 1px">
+      <div style="color: black">6 Direction. Random Angle</div>
+      <WordCloudTunnel
+        ref="el2"
+        :color="['#ffd077', '#3bc4c7', '#3a9eea', '#ff4e69', '#461e47']"
+        :axisCount="6"
+        :randomAngleRangePerAxis="60"
+        :randomAxis="false"
+        :scaleStep="0.15"
+        :animation-duration="1000"
+      ></WordCloudTunnel>
+    </div>
+
+    <div style="flex: 1 1 auto; border: solid #ddd 1px; display: flex; flex-direction: column">
+      <div style="color: black">6 Direction. No Random Angle</div>
+      <div style="flex: 1 1 auto">
+        <WordCloudTunnel
+          ref="el4"
+          :color="['#ffd077', '#3bc4c7', '#3a9eea', '#ff4e69', '#461e47']"
+          :axisCount="6"
+          :randomAngleRangePerAxis="0"
+          :randomAxis="false"
+          :scaleStep="0.2"
+          :animation-duration="1000"
+        ></WordCloudTunnel>
+      </div>
+      <div @click="handAdd" style="flex: 0; color: black; cursor: pointer">Add Item</div>
+    </div>
+
+    <div style="flex: 1 1 auto; border: solid #ddd 1px; display: flex; flex-direction: column">
+      <div style="color: black">
+        4 Direction. No Random Angle,. 3 Level with eath level 4 items. add item by hand.
+      </div>
+      <div style="flex: 1 1 auto">
+        <WordCloudTunnel
+          ref="el"
+          :color="['#ffd077', '#3bc4c7', '#3a9eea', '#ff4e69', '#461e47']"
+          :deeps="[4, 4, 4]"
+          :axisCount="4"
+          :randomAngleRangePerAxis="0"
+          :randomAxis="false"
+          :scaleStep="0.2"
+          :animation-duration="1000"
+        ></WordCloudTunnel>
+      </div>
+      <div @click="handAdd" style="flex: 0; color: black; cursor: pointer">Add Item</div>
+    </div>
   </div>
 
-  <div style="width: 600px; height: 400px; border: solid #ddd 1px">
+  <div style="width: 800px; height: 600px; border: solid #ddd 1px">
+    <div style="color: black">Custom Item Slot</div>
     <WordCloudTunnel
-      ref="el"
+      ref="el3"
       :color="['#ffd077', '#3bc4c7', '#3a9eea', '#ff4e69', '#461e47']"
       :axisCount="6"
       :randomAngleRangePerAxis="60"
       :randomAxis="false"
-      :scaleStep="0.15"
+      :scaleStep="0"
+      :opacityStep="0"
       :animation-duration="1000"
-    ></WordCloudTunnel>
+    >
+      <template #default="item">
+        <div :style="customItemStyle(item)">
+          {{ item.text }}
+        </div>
+      </template>
+    </WordCloudTunnel>
   </div>
-  <div @click="randomAdd" style="color: black; cursor: pointer">Random add</div>
 </template>
 
 <style>
